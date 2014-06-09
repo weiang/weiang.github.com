9ab812b6dfd5da6f857288344117f631e2535700
weiang <weiang@mail.ustc.edu.cn>
2014-06-09 21:44:57 +0800

    Research Statement changed!

---
+++ index.md.old.rs
@@ -0,0 +1,25 @@
+---
+layout: page
+title: About Me
+tagline: Minimal Mistakes, a Jekyll Theme
+tags: [about, Jekyll, theme, responsive]
+modified: 1-5-2014
+link: http://www.ustc.edu.cn
+comments: true
+image:
+  feature: texture-feature-02.jpg
+  credit: Texture Lovers
+  creditlink: http://texturelovers.com
+---
+
+My name is Ang Wei. Now I'm a third year undergraduate student at [University of Science and Technology of China](http://www.ustc.edu.cn). My major is computer science and technology.
+
+# Research Interests
+My research interests are in the area of programming languages and software engineering. In particular, I am interested in following fields:
+
+- Domain-Specific Language design and implementation
+- Imporving security and reliability of softwares through programming language technology.
+
+Here is my [CV](../pdf/AngWei-CV.pdf).
+
+
